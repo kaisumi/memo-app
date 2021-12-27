128d0243cf62c1d92a9186ba4737dd7122119d90
kaisumi <kaisumi@langue-lab.com>
2021-12-27 10:35:42 +0900

    タイトルファイル廃止、エスケープをerbに移行

--- sinatra-webapp/memos.rb
+++ sinatra-webapp/memos.rb
@@ -5,6 +5,8 @@ require 'cgi'
 
 NO_TITLE_ERROR = '<font color="red">タイトルが空欄です</font>'
 MEMO_ID_DIGIT = 10
+STORAGE = 'memos'
+LATEST_ID = './memo_latest_id.txt'
 
 get '/' do
   @memo_titles = read_titles
@@ -27,14 +29,12 @@ post '/memos' do
     @message = ''
     id_text = new_id
     write_memo(id_text, params)
-    write_titles(id_text, params)
     redirect to('/')
   end
 end
 
 delete %r{/memos/([0-9]{10})} do |id_text|
   delete_memo(id_text)
-  delete_title(id_text)
   redirect to('/')
 end
 
@@ -53,63 +53,61 @@ patch %r{/memos/([0-9]{10})} do |id_text|
   if params[:title].empty?
     @title_blank = true
     @memo_contents = params
-    @memo_contents[:index] = id_text
+    @memo_contents[:memo_id] = id_text
     @message = NO_TITLE_ERROR
     erb :editor
   else
     @message = ''
     write_memo(id_text, params)
-    update_title(id_text, params)
-    redirect to("/memos/#{id_text}")
+    redirect to("/#{STORAGE}/#{id_text}")
   end
 end
 
-
 def write_memo(id_text, params)
-  Dir.mkdir("memos") if !Dir.exist?("memos")
-  File.open("./memos/#{id_text}.txt", 'w') do |file|
-    file.puts "#{id_text}\n#{CGI.escapeHTML(params[:title])}\n#{CGI.escapeHTML(params[:body])}"
+  Dir.mkdir(STORAGE) unless Dir.exist?(STORAGE)
+  File.open("./#{STORAGE}/#{id_text}.txt", 'w') do |file|
+    file.puts "#{id_text}\n#{params[:title]}\n#{params[:body]}"
   end
 end
 
 def new_id
   id_text = ''
-  File.open('./memo_latest_id.txt', 'r') do |file|
+  File.open(LATEST_ID, 'r') do |file|
     id_text = format("%0#{MEMO_ID_DIGIT}d", file.read.to_i + 1)
   end
-  File.open('./memo_latest_id.txt', 'w') do |file|
+  File.open(LATEST_ID, 'w') do |file|
     file.puts id_text
   end
   id_text
 end
 
 def read_titles
-  memos = []
-  File.open('./titles.txt', 'r') do |file|
-    memos = \
-      file.read.split("\n").map do |memo|
-        next if memo.empty?
-
-        memo_id, title = memo.split(', ')
-        { memo_id: memo_id, title: title }
+  id_text = ''
+  memo_titles = []
+  File.open(LATEST_ID, 'r') do |file|
+    id_text = file.read.delete("\n")
+  end
+  unless id_text.to_i.zero?
+    1.upto(id_text.to_i) do |id_integer|
+      memo_id = format("%0#{MEMO_ID_DIGIT}d", id_integer)
+      filename = "./#{STORAGE}/#{memo_id}.txt"
+      if File.exist?(filename)
+        contents = read_memo_contents(memo_id)
+        memo_titles << { memo_id: contents[:memo_id], title: contents[:title] }
       end
+    end
   end
-  memos
-end
-
-def write_titles(id_text, params)
-  File.open('./titles.txt', 'a') do |file|
-    file.puts "#{id_text}, #{params[:title]}"
-  end
+  memo_titles
 end
 
 def read_memo_contents(memo_id)
   contents = {}
-  File.open("./memos/#{memo_id}.txt", 'r') do |file|
+  filename = "./#{STORAGE}/#{memo_id}.txt"
+  File.open(filename, 'r') do |file|
     body = ''
     file.read.split("\n").each_with_index do |content, i|
       case i
-      when 0 then contents.store(:index, content)
+      when 0 then contents.store(:memo_id, content)
       when 1 then contents.store(:title, content)
       else
         body += "#{content}\n"
@@ -121,29 +119,5 @@ def read_memo_contents(memo_id)
 end
 
 def delete_memo(id_text)
-  File.delete("./memos/#{id_text}.txt")
-end
-
-def delete_title(id_text)
-  titles = []
-  File.open('./titles.txt', 'r') do |file|
-    file.read.lines do |line|
-      titles << line if line.index(id_text).nil?
-    end
-  end
-  File.open('./titles.txt', 'w') do |file|
-    file.puts titles.join
-  end
-end
-
-def update_title(id_text, params)
-  titles = []
-  File.open('./titles.txt', 'r') do |file|
-    file.read.lines do |line|
-      titles << (line.index(id_text).nil? ? line : "#{id_text}, #{CGI.escapeHTML(params[:title])}\n")
-    end
-  end
-  File.open('./titles.txt', 'w') do |file|
-    file.puts titles.join
-  end
+  File.delete("./#{STORAGE}/#{id_text}.txt")
 end
