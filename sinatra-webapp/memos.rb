97ec572b249729327ed235fba6590a5904040fa5
kaisumi <kaisumi@langue-lab.com>
2022-01-11 12:49:45 +0900

    リファクタリング

--- sinatra-webapp/memos.rb
+++ sinatra-webapp/memos.rb
@@ -7,7 +7,7 @@ require_relative 'error_check'
 require_relative 'constant_parameters'
 
 get '/' do
-  @memo_titles = read_titles
+  @memo_titles = sql_interaction(:read_titles)
   erb :index
 end
 
@@ -20,7 +20,7 @@ end
 post '/memos' do
   @error_check = ErrorCheck.new(params)
   if @error_check.status
-    add_memo(params)
+    sql_interaction(:insert, params: params)
     redirect to('/')
   else
     @memo_contents = params
@@ -29,25 +29,25 @@ post '/memos' do
 end
 
 delete %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})} do |id_text|
-  delete_memo(id_text)
+  sql_interaction(:delete, id_integer: id_text.to_i)
   redirect to('/')
 end
 
 get %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})} do |id_text|
-  @memo_contents = read_memo_contents(id_text)
+  @memo_contents = sql_interaction(:read_contents, id_integer: id_text.to_i)
   erb :memo_contents
 end
 
 get %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})/editor} do |id_text|
   @error_check = ErrorCheck.new
-  @memo_contents = read_memo_contents(id_text)
+  @memo_contents = sql_interaction(:read_contents, id_integer: id_text.to_i)
   erb :editor
 end
 
 patch %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})} do |id_text|
   @error_check = ErrorCheck.new(params)
   if @error_check.status
-    update_memo(id_text, params)
+    sql_interaction(:update, params: params, id_integer: id_text.to_i)
     redirect to(memo_url(id_text))
   else
     @memo_contents = params
@@ -56,55 +56,48 @@ patch %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})} do |id_text|
   end
 end
 
-def add_memo(params)
-  conn = connect_db
-  conn.exec(sql_insert(params[:title], params[:body]))
+def sql_interaction(command, params: nil, id_integer: nil)
+  sql_text = generate_sql(command, params, id_integer)
+  execute_sql(command, sql_text)
 end
 
-def update_memo(id_text, params)
-  conn = connect_db
-  conn.exec(sql_update(params[:title], params[:body], id_text.to_i))
-end
-
-def read_titles
-  memos = []
-  conn = connect_db
-  conn.exec("SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL} FROM #{TABLE_NAME}") do |result|
-    memos = result_into_array(result)
+def generate_sql(command, params = nil, id_integer = nil)
+  case command
+  when :insert
+    "INSERT INTO #{TABLE_NAME} (#{TITLE_COL}, #{BODY_COL}) VALUES ('#{params[:title]}', '#{params[:body]}')"
+  when :update
+    "UPDATE #{TABLE_NAME} SET #{TITLE_COL} = '#{params[:title]}', #{BODY_COL} = '#{params[:body]}' WHERE #{ID_COL} = #{id_integer}"
+  when :delete
+    "DELETE FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_integer}"
+  when :read_titles
+    "SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL} FROM #{TABLE_NAME}"
+  when :read_contents
+    "SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL}, #{BODY_COL} as #{BODY_COL} FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_integer}"
   end
-  memos
 end
 
-def read_memo_contents(id_text)
-  contents = {}
-  conn = connect_db
-  conn.exec("SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL}, #{BODY_COL} as #{BODY_COL} FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_text.to_i}") do |result|
-    contents = result[0]
+def execute_sql(command, sql_text)
+  contents = nil
+  conn = PG.connect(dbname: DB_NAME)
+  conn.field_name_type = :symbol
+
+  case command
+  when :read_titles
+    conn.exec(sql_text) do |result|
+      contents = result_into_array(result)
+    end
+  when :read_contents
+    conn.exec(sql_text) do |result|
+      contents = result[0]
+    end
+  else
+    conn.exec(sql_text)
   end
   contents
 end
 
-def delete_memo(id_text)
-  conn = connect_db
-  conn.exec("DELETE FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_text.to_i}")
-end
-
 def memo_url(id_text)
   "/memos/#{id_text}"
-end
-
-def sql_insert(title, body)
-  "INSERT INTO #{TABLE_NAME} (#{TITLE_COL}, #{BODY_COL}) VALUES ('#{title}', '#{body}')"
-end
-
-def sql_update(title, body, id_integer)
-  "UPDATE #{TABLE_NAME} SET #{TITLE_COL} = '#{title}', #{BODY_COL} = '#{body}' WHERE #{ID_COL} = #{id_integer}"
-end
-
-def connect_db
-  conn = PG.connect(dbname: DB_NAME)
-  conn.field_name_type = :symbol
-  conn
 end
 
 def result_into_array(result)
