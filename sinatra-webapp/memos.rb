63fd836d3be885f18bdb11d6181e919af9a4a8b4
kaisumi <kaisumi@langue-lab.com>
2022-01-12 13:11:41 +0900

    SQLインジェクション対策、DB接続をinitializeに移動、.to_aに対応

--- sinatra-webapp/memos.rb
+++ sinatra-webapp/memos.rb
@@ -57,41 +57,43 @@ patch %r{/memos/([0-9]{#{MEMO_ID_DIGIT}})} do |id_text|
 end
 
 def sql_interaction(command, params: nil, id_integer: nil)
-  sql_text = generate_sql(command, params, id_integer)
-  execute_sql(command, sql_text)
+  sql_params = []
+  sql_params << params['title'] << params['body'] unless params.nil?
+  sql_params << id_integer unless id_integer.nil?
+  sql_text = generate_sql(command)
+  execute_sql(command, sql_text, sql_params)
 end
 
-def generate_sql(command, params = nil, id_integer = nil)
+def generate_sql(command)
   case command
   when :insert
-    "INSERT INTO #{TABLE_NAME} (#{TITLE_COL}, #{BODY_COL}) VALUES ('#{params[:title]}', '#{params[:body]}')"
+    "INSERT INTO #{TABLE_NAME} (#{TITLE_COL}, #{BODY_COL}) VALUES ($1, $2)"
   when :update
-    "UPDATE #{TABLE_NAME} SET #{TITLE_COL} = '#{params[:title]}', #{BODY_COL} = '#{params[:body]}' WHERE #{ID_COL} = #{id_integer}"
+    "UPDATE #{TABLE_NAME} SET #{TITLE_COL} = $1, #{BODY_COL} = $2 WHERE #{ID_COL} = $3"
   when :delete
-    "DELETE FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_integer}"
+    "DELETE FROM #{TABLE_NAME} WHERE #{ID_COL} = $1"
   when :read_titles
     "SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL} FROM #{TABLE_NAME}"
   when :read_contents
-    "SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL}, #{BODY_COL} as #{BODY_COL} FROM #{TABLE_NAME} WHERE #{ID_COL} = #{id_integer}"
+    "SELECT #{MEMO_ID} as #{ID_COL}, #{TITLE_COL}, #{BODY_COL} as #{BODY_COL} FROM #{TABLE_NAME} WHERE #{ID_COL} = $1"
   end
 end
 
-def execute_sql(command, sql_text)
+def execute_sql(command, sql_text, sql_params)
   contents = nil
-  conn = PG.connect(dbname: DB_NAME)
-  conn.field_name_type = :symbol
+  @conn.field_name_type = :symbol
 
   case command
   when :read_titles
-    conn.exec(sql_text) do |result|
-      contents = result_into_array(result)
+    @conn.exec(sql_text) do |result|
+      contents = result.to_a
     end
   when :read_contents
-    conn.exec(sql_text) do |result|
+    @conn.exec_params(sql_text, sql_params) do |result|
       contents = result[0]
     end
   else
-    conn.exec(sql_text)
+    @conn.exec_params(sql_text, sql_params)
   end
   contents
 end
@@ -100,10 +102,6 @@ def memo_url(id_text)
   "/memos/#{id_text}"
 end
 
-def result_into_array(result)
-  memos = []
-  result.cmd_tuples.times do |i|
-    memos << result[i]
-  end
-  memos
+def initialize
+  @conn = PG.connect(dbname: DB_NAME)
 end
